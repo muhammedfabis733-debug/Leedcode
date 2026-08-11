377eb7175b5becc786cd60b09bfa3adba354d99e
Muhammed Fabis <muhammedfabis.733@gmail.com>
2026-08-11 09:59:27 +0530

    Time: 0 ms (100%), Space: 53.2 MB (83.33%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    let Kelvin=celsius + 273.15;
+    let Fahrenheit= celsius * 1.80 + 32.00
+
+    return[Kelvin,Fahrenheit];
+};
+console.log(convertTemperature(36.5))
