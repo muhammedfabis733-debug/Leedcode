4a81cf23f1d30baacc393aaf3f0d30b371a4c142
Muhammed Fabis <muhammedfabis.733@gmail.com>
2026-08-12 10:31:59 +0530

    Time: 0 ms (100%), Space: 53.6 MB (57.58%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+   return [n].map(num => num %2 === 0 ? num:num *2)[0];
+};
+console.log(smallestEvenMultiple(5))
+
