18151d362900ae23f2c09806bb225b22a6a8d333
Muhammed Fabis <muhammedfabis.733@gmail.com>
2026-09-07 09:43:00 +0530

    Time: 0 ms (100%), Space: 54.2 MB (25.22%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isThree = function(n) {
+   let count=0;
+
+   for (let i=1; i <= n;i++){
+    if( n % i===0){
+        count++;
+    }
+   } 
+   return count ===3;
+};
+
+console.log(isThree(2));
